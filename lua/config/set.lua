fb9abee4d7b6ebd3053a16dce05edbef7443dd25
Affapple <joaopmgaspar@ua.pt>
2025-07-04 20:12:25 +0100

    rust, not testes

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -2,26 +2,24 @@ vim.opt.nu = true
 vim.opt.relativenumber = true
 
 vim.opt.tabstop = 4
-vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = {
-		"html",
-		"typescript",
-		"tsx",
-		"jsx",
-		"typescriptreact",
-		"ts",
-		"js",
-		"css",
-		"lua",
-		"xml",
-		"typescript",
-		"javascript",
-	},
-	command = "setlocal shiftwidth=2 tabstop=2",
+  pattern = {
+    "html",
+    "css",
+    "ts",
+    "typescript",
+    "tsx",
+    "typescriptreact",
+    "js",
+    "javascript",
+    "jsx",
+    "lua",
+    "xml",
+  },
+  command = "setlocal shiftwidth=2 tabstop=2",
 })
 
 vim.opt.smartindent = true
@@ -49,10 +47,10 @@ vim.opt.colorcolumn = "80"
 vim.opt.cmdheight = 0
 
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "md", "txt", "markdown" },
-	callback = function()
-		vim.opt_local.wrap = true
-		vim.api.nvim_buf_set_keymap(0, "n", "j", "gj", { noremap = true, silent = true })
-		vim.api.nvim_buf_set_keymap(0, "n", "k", "gk", { noremap = true, silent = true })
-	end,
+  pattern = { "md", "txt", "markdown" },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.api.nvim_buf_set_keymap(0, "n", "j", "gj", { noremap = true, silent = true })
+    vim.api.nvim_buf_set_keymap(0, "n", "k", "gk", { noremap = true, silent = true })
+  end,
 })
