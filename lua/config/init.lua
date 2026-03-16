d80ff099d6b862c4d422c31aeef28ce1a55c1ad2
Affapple <joaopmgaspar@ua.pt>
2026-03-16 11:18:49 +0100

    fix typo

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -49,7 +49,7 @@ autocmd("LspAttach", {
 		vim.keymap.set("n", "<leader>vca", function()
 			vim.lsp.buf.code_action()
 		end, opts)
-		vim.keymap.set("n", "<levader>vrr", function()
+		vim.keymap.set("n", "<leader>vrr", function()
 			vim.lsp.buf.references()
 		end, opts)
 		vim.keymap.set("n", "<leader>vrn", function()
